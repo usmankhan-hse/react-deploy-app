bd29d01c1d8ec8158ed73a991c951b602b86c47e
usmankhan-hse <usmankhan.hse@gmail.com>
2024-01-23 09:30:47 +0400

    Add post

--- src/NewPost.js
+++ src/NewPost.js
@@ -1,7 +1,30 @@
 import React from "react";
 
 const NewPost = () => {
-  return <div className="main">Hi this is new post div</div>;
+  return (
+    <div className="main">
+      <h2>New Post</h2>
+      <form action="" onSubmit={(e) => e.preventDefault()}>
+        <label htmlFor="">Title:</label>
+        <input
+          type="text"
+          name="titleTextName"
+          id="titleTextId"
+          className="titleText"
+        />
+        <label htmlFor="">Post:</label>
+        <textarea
+          name="postTextName"
+          id="postTextId"
+          cols="30"
+          rows="5"
+        ></textarea>
+        <button type="submit" className="submit">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default NewPost;
